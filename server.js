dbf11b471bfbf1caf05c34f995b5857b0390b43e
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-04-12 13:22:01 -0400

    PORT fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds017672.mlab.com:17672/heroku_nls174b6", {
   useNewUrlParser: true,
   useFindAndModify: false
 });

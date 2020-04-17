0862f5c8b027f6620936d9f5f3d458595c6596a7
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-04-16 20:24:07 -0400

    offline bugfix

--- public/service-worker.js
+++ public/service-worker.js
@@ -59,4 +59,12 @@ self.addEventListener("fetch", function (evt) {
 
     return;
   }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
